e08efe7599defb303672ac05abb77b5e9d132b97
Aaron Bjork <aaronbjork@hotmail.com>
2019-05-01 08:06:50 -0700

    Adding the scoring average function.

---
+++ Models/Round.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace tgsc_functions.Models
+{
+    [System.Serializable]
+    public class Round
+    {
+        public int Score { get; set; }
+ 
+        public Round()
+        { }
+    }
+}
--- PlayerScoringAverage.cs
+++ PlayerScoringAverage.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,6 +8,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using RestSharp;
+using tgsc_functions.Models;
+using System.Linq;
 
 namespace tgsc_functions
 {
@@ -17,17 +21,22 @@ namespace tgsc_functions
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("The PlayerScoringAverage method was called.");
 
-            string name = req.Query["name"];
+            // Retreive the player id from the querystring.
+            string playerId = req.Query["playerid"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            // Call the tgsc API to return all rounds for this player.
+            var client = new RestClient("https://tgsc-api.azurewebsites.net/api");
+            var request = new RestRequest("players/" + playerId + "/rounds", DataFormat.Json);
+            var rounds = client.Get<List<Round>>(request).Data;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            // Determine the scoring average.
+            double scoringAvg = Math.Round(rounds.Average(r => r.Score), 1);
+
+            return playerId != null
+                ? (ActionResult)new OkObjectResult($"Hello, {playerId}, your scoring average is: " + scoringAvg.ToString())
+                : new BadRequestObjectResult("Please pass a player id on the query string or in the request body");
         }
     }
 }
